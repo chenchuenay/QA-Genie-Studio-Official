8f68d28e42d476a8607edb5bf3afc14c26bb4907
Enay Kumar <chenchuenay97@gmail.com>
2026-05-09 16:59:46 +0400

    remove implementation leakage and refine QA realism boundaries

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2.68"
+        versionName = "1.2.69"
         multiDexEnabled = true
     }
 
